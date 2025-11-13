367dbaea4d7d45e59c23417bb2ac553e90fa47cf
Lucas Ribeiro <ribeiro.lucas.soares@gmail.com>
2025-11-13 12:37:18 -0300

    feat: Auto backup

--- cmd_run_ytsearch.ps1
+++ cmd_run_ytsearch.ps1
@@ -1,6 +1,7 @@
-# conda deactivate
-# conda activate base
-C:\Lucas\Programas\ArcGISPro\envs\arcgispro-clone-pro-32\python.exe ytsearch.py
+conda deactivate
+conda activate base
+# C:\Lucas\Programas\ArcGISPro\envs\arcgispro-clone-pro-32\python.exe ytsearch.py
+python.exe ytsearch.py
 git add *
 git commit -m "feat: Auto backup"
 git push
